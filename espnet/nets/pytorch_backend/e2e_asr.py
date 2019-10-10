Fixed the oversampling masking
In the previous oversampling masking, due to dropout layer, the encoded
states were different for same inputs. thus the "delta_norm" calculated
was wrong. We fixed it here by calculating delta_norm right at the
input.
*asr_train.py -> added oversamp_epsilon as an arguement
*e2e-asr.py -> calculate the ignore_mask by evaluating norm of
	difference for inputs (in this case, hs_pad)
*decoders.py-> use ignore_mask to assing ignore_id to repeated
	transcripts thus ignoring the cross entropy loss calculated
	for that
batchfy_modified.py -> changed indent of sorted_data=temp_sorted as it
	was calculated for every iteration, thus extremenly slow. Also the
	sorted data json is dumped to /tmp folder

espnet/nets/pytorch_backend/e2e_asr.py
@@ -141,6 +141,7 @@ class E2E(ASRInterface, torch.nn.Module):
         self.pairwise = args.pairwise
         self.pair_lambda = args.pair_lambda
         self.pair_alpha = args.pair_alpha
+        self.epsilon = args.oversamp_epsilon
 
     def init_like_chainer(self):
         """Initialize weight like chainer
@@ -207,6 +208,17 @@ class E2E(ASRInterface, torch.nn.Module):
             hs_pad, hlens = self.feature_transform(hs_pad, hlens)
         else:
             hs_pad, hlens = xs_pad, ilens
+        ignore_mask = np.zeros(len(hs_pad))
+        if self.pairwise:
+            r,c = hs_pad[0,:,:].shape
+            for i in range(int(len(hs_pad)/2)):
+                delta_norm =torch.norm(hs_pad[2*i,:,:]-hs_pad[2*i+1,:,:])/pow(r*c,0.5)
+                logging.info("delta_norm is" + str(delta_norm))
+                #logging.info("epsilon is" + str(self.epsilon))
+                if( delta_norm <= self.epsilon):
+                    ignore_mask[2*i]=1
+        ignore_mask = (ignore_mask==1)
+        #logging.info("ignore_mask is "+ str(ignore_mask))
 
         # 1. Encoder
         #logging.info("before sorting hlens is: "+ str(hlens))
@@ -227,9 +239,9 @@ class E2E(ASRInterface, torch.nn.Module):
 
         # 3. attention loss
         if self.mtlalpha == 1:
-            self.loss_att, acc = None, None
+            self.loss_att, self.loss_siam, acc = None, None, None
         else:
-            self.loss_att, self.loss_siam, acc = self.dec(hs_pad, hlens, ys_pad, perm_index)
+            self.loss_att, self.loss_siam, acc = self.dec(hs_pad, hlens, ys_pad, ignore_mask)
         self.acc = acc
 
         # 4. compute cer without beam search
@@ -282,7 +294,7 @@ class E2E(ASRInterface, torch.nn.Module):
                 seq_hat_text = seq_hat_text.replace(self.recog_args.blank, '')
                 seq_true_text = "".join(seq_true).replace(self.recog_args.space, ' ')
 
-				#print added by vinit to compare between teacher forced and non teacher forced outputs
+                #print added by vinit to compare between teacher forced and non teacher forced outputs
                 logging.info('Nbest output-->groundtruth is ' + seq_true_text)
                 logging.info('Nbest output-->prediction  is ' + seq_hat_text)
 

espnet/nets/pytorch_backend/rnn/decoders.py
@@ -111,7 +111,7 @@ class Decoder(torch.nn.Module):
                 z_list[l] = self.decoder[l](self.dropout_dec[l - 1](z_list[l - 1]), z_prev[l])
         return z_list, c_list
 
-    def forward(self, hs_pad, hlens, ys_pad, perm_index, strm_idx=0,epsilon=1e-10):
+    def forward(self, hs_pad, hlens, ys_pad, ignore_mask, strm_idx=0):
         """Decoder forward
 
         :param torch.Tensor hs_pad: batch of padded hidden state sequences (B, Tmax, D)
@@ -202,12 +202,12 @@ class Decoder(torch.nn.Module):
         lens = [y.size(0) for y in ys_out]
             
         #Adding below line to remove oversampling due repeated files        
-        #TODO: mSet approriate value of epsilon
-        for i in range(int(len(hs_pad)/2)):
-            delta_norm =torch.norm(hs_pad[2*i,:,:]-hs_pad[2*i+1,:,:]) 
-           # logging.info("delta_norm is" + str(delta_norm))
-            if( delta_norm <= epsilon):
-                ys_out_pad[2*i,:]=self.ignore_id
+        #logging.info("shape of ys_out_pad is" + str(ys_out_pad.shape)) 
+        #logging.info('ys_out_pad_before is ' + str(ys_out_pad))
+        for i in range(len(ignore_mask)):
+            if ignore_mask[i]:
+                ys_out_pad[i,:]=self.ignore_id
+        #logging.info('ys_out_pad_after is ' + str(ys_out_pad))
         
         
         self.loss = F.cross_entropy(y_all, ys_out_pad.view(-1),

espnet/utils/training/batchfy_modified.py
@@ -1,5 +1,6 @@
 import itertools
 import logging
+import json
 
 import numpy as np
 
@@ -392,9 +393,11 @@ def make_batchset(data, batch_size=0, max_length_in=float("inf"), max_length_out
             for i in np.random.permutation(range(len(temp_p))):
                 temp_sorted.append(temp_p[i])
                 temp_sorted.append(temp_q[i])
-                sorted_data = temp_sorted
+            sorted_data = temp_sorted
+            with open("/tmp/0.25_paired_dump",'w') as writer:
+                json.dump(sorted_data,writer)
 
-#            logging.info('sorted_data '+ str(sorted_data))
+            #logging.info('sorted_data '+ str(sorted_data))
 
 ##End of modif for pairwise
 
@@ -433,6 +436,9 @@ def make_batchset(data, batch_size=0, max_length_in=float("inf"), max_length_out
         batches = list(itertools.chain(*batches_list))
 
     # for debugging
+    #num_batches=300
+    logging.info('num_batches is'+str(num_batches))
+    
     if num_batches > 0:
         batches = batches[:num_batches]
     logging.info('# minibatches: ' + str(len(batches)))